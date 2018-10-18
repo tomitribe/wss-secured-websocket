cdc9e10039b9ad8a4a86ba723a056c913406cc9d
brunobat <brunobat@gmail.com>
2018-10-18 19:40:39 +0100

    use self signed certificate as default

--- src/test/java/org/superbiz/api/WebSocketResourceJEE7Test.java
+++ src/test/java/org/superbiz/api/WebSocketResourceJEE7Test.java
@@ -1,14 +1,21 @@
 package org.superbiz.api;
 /*
- * Tomitribe Confidential
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- * Copyright Tomitribe Corporation. 2018
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * The source code for this program is not published or otherwise divested
- * of its trade secrets, irrespective of what has been deposited with the
- * U.S. Copyright Office.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
-
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
@@ -113,8 +120,12 @@ public class WebSocketResourceJEE7Test {
                 .connectToServer(
                         endpoint,
                         authorizationConfiguration,
-                        new URI("wss", uri.getUserInfo(), "www.example.org", PORT, "/example/socket",
-                                null, null));
+//                      When using the keystore.jks.ca-cert certificate
+//                      new URI("wss", uri.getUserInfo(), "www.example.org", PORT, "/example/socket",
+//                              null, null)
+                        new URI("wss", uri.getUserInfo(), "localhost", PORT, "/example/socket",
+                                null, null)
+                );
 
         latch.await(1, TimeUnit.MINUTES);
         session.close();
